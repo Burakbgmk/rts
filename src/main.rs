1bedb0bf6dcc5150b5d715a8ba27059d6e83f7ed
Burak Boğmak <burakbogmak@gmail.com>
2023-12-29 00:28:29 +0300

    feat: added rate monotonic

--- src/main.rs
+++ src/main.rs
@@ -34,27 +34,44 @@ fn main() {
         turn_count: 0,
     };
 
-    let arr = vec![&mut first, &mut second, &mut third, &mut fourth];
+    let mut arr = vec![&mut first, &mut second, &mut third, &mut fourth];
+    arr.sort_by_key(|x| x.period);
     // rate_monotonic(&mut arr);
-    earliest_deadline(arr)
+    execute_algoritm("Earliest Deadline", &mut arr);
 
-    // calc_lcm(&mut nums);
+    let mut first = Task {
+        id: 1,
+        duration: 2,
+        period: 10,
+        turn_count: 0,
+    };
+    let mut second = Task {
+        id: 2,
+        duration: 3,
+        period: 15,
+        turn_count: 0,
+    };
+    let mut third = Task {
+        id: 3,
+        duration: 3,
+        period: 10,
+        turn_count: 0,
+    };
+    let mut fourth = Task {
+        id: 4,
+        duration: 4,
+        period: 30,
+        turn_count: 0,
+    };
+
+    arr = vec![&mut first, &mut second, &mut third, &mut fourth];
+    arr.sort_by_key(|x| x.duration);
+
+    execute_algoritm("Rate monotonic", &mut arr);
 }
 
-// fn rate_monotonic(arr: &mut Vec<&Task>) {
-//     arr.sort_by_key(|task| task.duration);
-//
-//     for task in arr {
-//         println!("{:?}", task);
-//         println!("Task {} starts...", task.id);
-//
-//         sleep(Duration::from_secs(task.duration as u64));
-//         println!("Task ends!");
-//     }
-// }
-
-fn earliest_deadline(mut arr: Vec<&mut Task>) {
-    println!("Earliest Deadline.....");
+fn execute_algoritm(algoritm_name: &str, arr: &mut Vec<&mut Task>) {
+    println!("{}....", algoritm_name);
     let mut periods: Vec<usize> = arr.iter().map(|x| x.period).collect();
     let lcm = calc_lcm(&mut periods).pop().unwrap();
     println!("lcm : {}", lcm);
@@ -63,7 +80,7 @@ fn earliest_deadline(mut arr: Vec<&mut Task>) {
     let mut current_time = 0;
     let mut current_count = 0;
     while current_time <= lcm {
-        let mut task_index = 0;
+        let mut task_index: usize;
         let current_task_index = arr
             .iter()
             .position(|x| x.turn_count == current_count && current_time >= x.period * x.turn_count);
@@ -71,19 +88,13 @@ fn earliest_deadline(mut arr: Vec<&mut Task>) {
         if current_task_index == None {
             let mut is_legit = false;
             while task_index < arr.len() {
-                // println!(
-                //     "task index : {}, current count : {}, current time {}",
-                //     task_index, current_count, current_time
-                // );
                 if current_time >= arr[task_index].period * arr[task_index].turn_count {
                     is_legit = true;
-                    // println!("legittt!");
                     break;
                 }
                 task_index += 1;
             }
             if !is_legit {
-                // println!("isnt legit!");
                 current_time += 1;
                 continue;
             }
